1b434233ce6be7608cd5e9b97bfc538c1dfa1ef3
MMMayC <changingvan@gmail.com>
2017-05-25 16:37:11 +1000

    Overall: Most/Least Revisions
    -Controller
    -Model
    -View

--- app/controllers/featuredwiki.server.controller.js
+++ app/controllers/featuredwiki.server.controller.js
@@ -1,46 +1,35 @@
 var Featuredwiki = require("../models/featuredwiki.js")
-var Async = require('async')
+var async = require('async')
 
 module.exports.showIndex=function(req,res){
 	var mostRevisions, leastRevisions, mostUsers, leastUsers;
-	Featuredwiki.findMostRevisions(function(err,result){
-		if (err){
-			console.log("Error")
-		}else{
-			console.log(result)
-			mostRevisions = result
-			console.log(mostRevisions[0]._id)
-			res.render("featuredwiki.pug", {mostRevisions:mostRevisions[0]._id})
-		}	
-	})
-	Featuredwiki.findLeastRevisions(function(err,result){
-		if (err){
-			console.log("Error")
-		}else{
-			console.log(result)
-			leastRevisions = result
-			console.log(leastRevisions[0]._id)
-			res.render("featuredwiki.pug", {mostRevisions:mostRevisions[0]._id})
-		}	
-	})
+	async.series([
+	           function(callback){
+	          	Featuredwiki.findMostRevisions(function(err,result){
+	        		if (err){
+	        			console.log("Error")
+	        		}else{
+	        			console.log(result)
+	        			mostRevisions = result
+	        			console.log(mostRevisions[0]._id)
+	                    callback();
+	        		}	
+	        	})},
+		        function(callback){
+	        	Featuredwiki.findLeastRevisions(function(err,result){
+	        		if (err){
+	        			console.log("Error")
+	        		}else{
+	        			console.log(result)
+	        			leastRevisions = result
+	        			console.log(leastRevisions[0]._id)
+	                    callback();
+	        		}	
+	        	})}      
+	],function(err,result){ 
+		res.render("featuredwiki.pug", {mostRevisions:mostRevisions[0]._id,leastRevisions:leastRevisions[0]._id});
+    });
+
 	
 	
-}
-
-
-
-module.exports.getLatest=function(req,res){
-	title = req.query.title
-
-	Featuredwiki.findTitleLatestRev(title, function(err,result){
-		
-		if (err){
-			console.log("Cannot find " + title + ",s latest revision!")
-		}else{
-			console.log(result)
-			revision = result[0]
-			res.render('revision.pug',{title: title, revision:revision})
-		}	
-	})	
-
 }
